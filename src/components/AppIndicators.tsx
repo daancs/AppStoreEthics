664378812f4756c3de2322d54ef65e4b6fddb8ce
angelixa <julia.bockert@outlook.com>
2023-03-26 16:29:22 +0200

    small css change

--- src/components/AppIndicators.tsx
+++ src/components/AppIndicators.tsx
@@ -3,7 +3,7 @@ import GameIcon from "./GameIcon";
 export default function AppIndicators(props: any) {
     return (
       <>
- <div className={`flex col-start-2 col-end-2 row-start-1 row-end-1 justify-evenly items-center w-full h-full font-mono`}>
+ <div className={`w-[450px] flex col-start-2 col-end-2 row-start-1 row-end-1 justify-evenly items-center h-full font-mono`}>
                 <GameIcon hoverable={true}label={"reputation"} />
                 <GameIcon hoverable={true} label={"contentment"}/>
                 <GameIcon  hoverable={true} label={"privacy"}/>
