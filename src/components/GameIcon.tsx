e00f8e94e0f103f2dcdbc4b2a5219c5b3dfe3cca
angelixa <julia.bockert@outlook.com>
2023-03-26 14:44:45 +0200

    created separate components for the two indicator-bars

--- src/components/GameIcon.tsx
+++ src/components/GameIcon.tsx
@@ -1,7 +1,7 @@
 import ProgressBar from '@/components/Progress';
 
 interface GameIconProps {
-    iconName: string;
+    iconPicture: string;
     width?: string;
     height?: string;
     progressValue?: number;
@@ -15,9 +15,10 @@ interface GameIconProps {
  */
 export default function GameIcon(props: GameIconProps) {
     return (
-            <div className={`relative ${props.width ? props.width : 'w-20'} ${props.height ? props.height : 'h-20'} bg-white rounded-xl shadow-xl text-black text-xl transition-all ease-in-out delay-100 hover:scale-110 hover:cursor-pointer overflow-hidden`}>
+        //Removed hover:scale-110 hover:cursor-pointer, only relevant in AppIndicators, not ConsIndicators
+            <div className={`relative ${props.width ? props.width : 'w-20'} ${props.height ? props.height : 'h-20'} bg-white rounded-xl shadow-xl text-black text-xl transition-all ease-in-out delay-100 overflow-hidden`}>
                 <div className="text-center">
-                    {props.iconName}
+                    {props.iconPicture}
                 </div>
                 <div className='absolute bottom-0 w-full'>
                     {props.displayProgress ? <ProgressBar value={Number(props.progressValue) || 50} parentHeight={`${props.height ? props.height : '20'}`}/> : null} 
