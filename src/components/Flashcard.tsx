25dd4acccaa9cac25e934cf6c318c12a339226e9
steffi0929 <steffi.gumman@hotmail.com>
2023-03-29 14:03:52 +0200

    safari bug fixed and changed text

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -36,20 +36,19 @@ export default function Flashcard(props: FlashcardProps) {
     return (
             <div className='h-[60vh] w-[25vw] min-h-[60vh] min-w-[400px] [perspective:1000px] hover:cursor-pointer transition-all ease-in-out duration-500 hover:scale-105'>
                 <div className={`relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] ${flip ? '[transform:rotateY(180deg)]' : ''}`} onClick={() => handleOnClick()}>
-                    <div className={`relative inset-0 rounded-xl shadow-xl bg-slate-100`}>
+                    <div className={`relative inset-0 rounded-xl shadow-xl bg-slate-100 [backface-visibility:hidden]`}>
                         <div className={`flex items-center justify-center`}>
                             <Image src={mySpyImage ?? 'App Picture'} alt='App Picture not found' className={`rounded-xl ${props.width ? props.width : 'w-[300px]'} ${props.height ? props.height : 'h-[60vh]'} max-h-full min-h-full`}/>
                         </div>              
                     </div>
                     <div className='absolute inset-0 h-full w-full rounded-xl shadow-xl bg-slate-100 px-10 text-black [transform:rotateY(180deg)] [backface-visibility:hidden]'>
                         <div className='flex flex-col rounded-xl'>
-                            <div className='text-3xl py-8 text-center'>
+                            <div className='text-2xl py-6 text-center'>
                                 {mySpyApp.title}
                             </div>
-                            <span className='text-lg'>
+                            <span className='text-base'>
                                 Users of this app can:
-                                <br/><br/>
-                                <div className='whitespace-pre-line min-h-[40vh] h-[300px] overflow-scroll px-4'>
+                                <div className='whitespace-pre-line h-fit px-4 text-sm py-2'>
                                     {mySpyApp.description}
                                 </div>
                             </span>
