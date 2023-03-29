4effb18242c55961f4295d5eb73b2dd5fcb2ba93
steffi0929 <steffi.gumman@hotmail.com>
2023-03-29 15:03:09 +0200

    final end page

---
+++ src/pages/finalEnd.tsx
@@ -0,0 +1,32 @@
+import { NextPage } from 'next';
+import { Roboto_Mono } from '@next/font/google'
+
+const roboto = Roboto_Mono({ subsets: ['latin'], variable: '--font-roboto-mono' })
+
+
+const FinalEnd: NextPage = () => {
+
+    const endText = {
+        greeting: "Thank you for playing our game!\n\n",
+        content: "We would appreciate it if you would answer some questions regarding the game :)\n\n Here's the ",
+        linkText: "link"
+    }
+
+    const questionsLink = "https://www.youtube.com/watch?v=dQw4w9WgXcQ;autoplay=1;fs=0;autohide=0;hd=0;";
+
+    return (
+        <>
+            <div className='w-screen h-screen flex flex-col justify-center items-center bg-primary-bg text-white'>
+                <div className='flex flex-col'>
+                    <div className={`${roboto.variable} text-4xl font-mono m-2`}>{endText.greeting}</div>
+                        <div className='font-mono m-2 max-w-lg whitespace-pre-line'>
+                            {endText.content}
+                            <a href={`${questionsLink}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{endText.linkText}</a>
+                        </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default FinalEnd;
