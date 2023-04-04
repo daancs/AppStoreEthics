9a7ce1f66205ce3ffdd85ce1dfdd0b4c68a4abc8
ember97 <emmieberger@live.se>
2023-04-04 15:17:48 +0200

    Deleted some unintended changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link.js";
-import { useRouter } from "next/router";
-import React from "react";
 
 interface ButtonProps {
     children: React.ReactNode;
     link: string;
-    shortcut: string;
     bgColor?: string;
     textColor?: string;
 }
@@ -17,32 +14,13 @@ interface ButtonProps {
  * @returns a button wich links to a page
  */
 const Button = (props: ButtonProps) => {
-    const router = useRouter();
-    
-    // Go to link when shortcut pushed
-    const handleKeyDown = () => {
-        console.log("Key pressed: " + props.shortcut)
-        router.push('/' + props.link);
-    };
-
-
-    // Add when loading page
-    React.useEffect(() => {
-    window.addEventListener(props.shortcut, handleKeyDown);
-    
-    // Removing when leaving page
-    return () => {
-      window.removeEventListener(props.shortcut, handleKeyDown);
-    };
-    }, []);
-
     return (
         <>
-        <Link href={`/${props.link}`}>
-            <button id = {props.shortcut} className={`text-start tracking-wide w-full text-3xl rounded-xl py-2 px-8 hover:bg-slate-400 ${props.bgColor ? props.bgColor : 'bg-slate-100'} ${props.textColor ? props.textColor : 'text-black'}`}>
-                {props.children}
-            </button>
-        </Link>
+            <Link href={`/${props.link}`}>
+                <button className={`text-start tracking-wide w-full text-3xl rounded-xl py-2 px-8 hover:bg-slate-400 ${props.bgColor ? props.bgColor : 'bg-slate-100'} ${props.textColor ? props.textColor : 'text-black'}`}>
+                    {props.children}
+                </button>
+            </Link>
         </>
     );
 }
