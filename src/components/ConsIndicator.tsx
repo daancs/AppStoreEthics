724da09a7be872f05a2ac174ebb5ea8789ee551e
williamProgrammerar <william@johnston.se>
2023-04-17 09:59:06 +0200

    Unchanged icon progress is now displayed in blue

--- src/components/ConsIndicator.tsx
+++ src/components/ConsIndicator.tsx
@@ -8,10 +8,12 @@ export interface Indicator{
 }
 
 export default function ConsIndicator(prop: Indicator){
+    const hasChanged = prop.change!=0;
+
     return (
     <div className="flex direction flex-col items-center">
-        <Arrow pointUp={prop.change>0} visible={prop.change!=0}/>
-        <GameIcon iconName={prop.name} progressValue={prop.value} displayProgress={true} hoverable={false}/>
+        <Arrow pointUp={prop.change>0} visible={hasChanged}/>
+        <GameIcon iconName={prop.name} isUnchanged={!hasChanged} progressValue={prop.value} displayProgress={true} hoverable={false}/>
         <span>{prop.name.charAt(0).toUpperCase()}</span>
     </div>
     );
